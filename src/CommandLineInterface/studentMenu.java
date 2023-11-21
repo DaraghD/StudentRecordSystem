f78825ccec3711328103f614b25b75248de432da
Caylum <caylumhurley@gmail.com>
2023-11-21 17:26:35 +0000

    Initial changes made to studentMenu class

--- src/CommandLineInterface/studentMenu.java
+++ src/CommandLineInterface/studentMenu.java
@@ -1,7 +1,22 @@
 package CommandLineInterface;
-
+import java.util.Scanner;
 public class studentMenu {
+    static Scanner input = new Scanner(System.in);
+
+    public void run() {
+        System.out.print("(Q)CA, (G)rades");
+        String choice = Scanner.nextLine();
+
+        if (choice = Q) {
+
+        } else if (choice = G) {
+
+        } else {
+            System.out.print("Invalid Input")
+        }
 
 
-
+    }
 }
+    //Display QCA, Display QCA,
+
