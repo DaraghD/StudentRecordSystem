4607ce5bac6f34e8e98ab253d9413b73aa856c7f
Caylum <caylumhurley@gmail.com>
2023-11-21 23:58:37 +0000

    Changes to studentMenu

--- src/CommandLineInterface/studentMenu.java
+++ src/CommandLineInterface/studentMenu.java
@@ -4,16 +4,19 @@ public class studentMenu {
     static Scanner input = new Scanner(System.in);
 
     public void run() {
-        System.out.print("(Q)CA, (G)rades");
+        System.out.print("(Q)CA, (G)rades, (M)odules");
         String choice = input.nextLine().toUpperCase();
 
         if (choice.equals("Q")) {
 
         } else if (choice.equals("G")) {
 
+        } else if (choice.equals("M")){
+
         } else {
             System.out.print("Invalid Input");
         }
+
 
 
     }
