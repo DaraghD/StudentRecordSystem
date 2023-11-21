20982790727257a4c4a1fd8b09253316760e8fb0
JoeC <joeconsidine9@gmail.com>
2023-11-21 19:09:46 +0000

    added teacherMenu

--- src/CommandLineInterface/teacherMenu.java
+++ src/CommandLineInterface/teacherMenu.java
@@ -1,6 +1,49 @@
 package CommandLineInterface;
+import java.util.Scanner;
+import java.util.Scanner.*;
 
 public class teacherMenu {
-//Joes Section
-    //e
+    private Scanner scannerTeacherMenu;
+    static Scanner input = new Scanner(System.in);
+
+    public teacherMenu() {
+        this.scannerTeacherMenu = new Scanner(System.in);
+    }
+
+    public void display() {
+        System.out.println("Teacher Menu" + "\n");
+
+        System.out.println("(G)Add Student Grades, (V)View Department Board, (D)Add Department, (L)ogout");
+    }
+
+    public void run() {
+        String choice;
+        do {
+
+            display();
+            System.out.println("Please enter an option" + "\n");
+            choice = scannerTeacherMenu.nextLine().toUpperCase();
+
+            switch (input.nextLine().toUpperCase()) {
+                case "G":
+                    addStudentGrade();
+                    break;
+                case "V":
+                    viewDepartmentBoard();
+                    break;
+                case "D":
+                    addDepartment;
+                    break;
+                case"L":
+                    System.out.println("Logging Out...");
+                    break;
+                default:
+                    System.out.println("Invalid Choice");
+            }
+        } while (choice != L);
+    }
+    public static void main (String[] args) {
+        teacherMenu teacherMenu = new teacherMenu;
+        teacherMenu.run();
+    }
 }
