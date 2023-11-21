809aa9d0e81b145c5656de9284f9cc3b331336ef
JoeC <joeconsidine9@gmail.com>
2023-11-21 23:14:21 +0000

    added teacherMenu

---
+++ src/CommandLineInterface/studentMenu.java
@@ -0,0 +1,22 @@
+package CommandLineInterface;
+import java.util.Scanner;
+public class studentMenu {
+    static Scanner input = new Scanner(System.in);
+
+    public void run() {
+        System.out.print("(Q)CA, (G)rades");
+        String choice = Scanner.nextLine().toUpperCase;
+
+        if (choice = "Q") {
+
+        } else if (choice = "G") {
+
+        } else {
+            System.out.print("Invalid Input");
+        }
+
+
+    }
+}
+//Display QCA, Display QCA,
+
