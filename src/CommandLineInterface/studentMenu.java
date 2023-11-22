1a9e68a7d9b5e1a47aa91468939eef9d86087943
Caylum <caylumhurley@gmail.com>
2023-11-22 00:05:59 +0000

    Added Logout option to studentMenu

--- src/CommandLineInterface/studentMenu.java
+++ src/CommandLineInterface/studentMenu.java
@@ -4,7 +4,7 @@ public class studentMenu {
     static Scanner input = new Scanner(System.in);
 
     public void run() {
-        System.out.print("(Q)CA, (G)rades, (M)odules");
+        System.out.print("(Q)CA, (G)rades, (M)odules, (L)ogout");
         String choice = input.nextLine().toUpperCase();
 
         if (choice.equals("Q")) {
@@ -12,6 +12,8 @@ public class studentMenu {
         } else if (choice.equals("G")) {
 
         } else if (choice.equals("M")){
+
+        } else if (choice.equals("L")){
 
         } else {
             System.out.print("Invalid Input");
