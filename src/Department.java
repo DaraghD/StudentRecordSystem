a243549d086816b2aba14de114e12290e353fd23
D-Money16 <22359621@studentmail.ul.ie>
2023-11-23 01:18:58 +0000

    Average QCA now calculated by module

--- src/Department.java
+++ src/Department.java
@@ -1,36 +1,59 @@
 import java.util.ArrayList;
-
 import Grading.Grade;
-import Person.*;
+import Person.Student;
 import University.University;
 
 public class Department {
 
     private University university;
 
-    public void calculateAndDisplayAverageQCA() {
-        System.out.println("Class average QCA:");
+    public void calculateAndDisplayAverageQCAForModule(String moduleCode) {
+        System.out.println("Class average QCA for module " + moduleCode + ":");
 
         double totalSum = 0.0;
         int totalGradesCount = 0;
 
+        // Iterate over students
         for (Student student : this.university.getStudents()) {
-
-           // System.out.println("Student " + student.getStudentId() + " QCA:");
-
+            // Iterate over grades of each student
             for (Grade grade : student.getGrades()) {
-                System.out.println("  QCA: " + grade.getGrade() + "|" + grade.convertGradeToNumber());
-                totalSum += grade.convertGradeToNumber();
-                totalGradesCount++;
+                // Check if the module matches the specified moduleCode
+                if (grade.getModule().equals(moduleCode)) {
+                    // Check the grade using if loops and convertGradeToNumber method
+                    if (grade.getGrade().equals("A1")) {
+                        totalSum += 4.0;
+                    } else if (grade.getGrade().equals("A2")) {
+                        totalSum += 3.6;
+                    } else if (grade.getGrade().equals("B1")) {
+                        totalSum += 3.2;
+                    } else if (grade.getGrade().equals("B2")) {
+                        totalSum += 3.0;
+                    } else if (grade.getGrade().equals("B3")) {
+                        totalSum += 2.8;
+                    } else if (grade.getGrade().equals("C1")) {
+                        totalSum += 2.6;
+                    } else if (grade.getGrade().equals("C2")) {
+                        totalSum += 2.4;
+                    } else if (grade.getGrade().equals("C3")) {
+                        totalSum += 2.0;
+                    } else if (grade.getGrade().equals("D1")) {
+                        totalSum += 1.6;
+                    } else if (grade.getGrade().equals("D2")) {
+                        totalSum += 1.2;
+                    } else if (grade.getGrade().equals("F")) {
+                        // For "F" grade, no addition to the totalSum
+                    }
+                    totalGradesCount++;
+                }
             }
         }
 
-        // Calculate and display the average of the entire class
+        // Calculate and display the average of the specified module
         if (totalGradesCount > 0) {
-            double classAverage = totalSum / totalGradesCount;
-            System.out.println("Class Average QCA: " + classAverage);
+            double moduleAverage = totalSum / totalGradesCount;
+            System.out.println("Average QCA for module " + moduleCode + ": " + moduleAverage);
         } else {
-            System.out.println("No grades available for the class.");
+            System.out.println("No grades available for the specified module.");
         }
     }
 }
