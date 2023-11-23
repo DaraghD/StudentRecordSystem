b396b29d951223f82a3ce29ea138dd8fba04b7ce
D-Money16 <22359621@studentmail.ul.ie>
2023-11-23 00:36:14 +0000

    Started Department class

--- src/Department.java
+++ src/Department.java
@@ -1,2 +1,29 @@
+import java.util.ArrayList;
+
 public class Department {
+
+    public void calculateAndDisplayAverageQCA(ArrayList<Student> students) {
+        System.out.println("Class average QCA:");
+
+        double totalSum = 0.0;
+        int totalGradesCount = 0;
+
+        for (Student student : students) {
+            System.out.println("Student " + student.getStudentId() + " QCA:");
+
+            for (Double grade : student.getGrades()) {
+                System.out.println("  QCA: " + grade);
+                totalSum += grade;
+                totalGradesCount++;
+            }
+        }
+
+        // Calculate and display the average of the entire class
+        if (totalGradesCount > 0) {
+            double classAverage = totalSum / totalGradesCount;
+            System.out.println("Class Average QCA: " + classAverage);
+        } else {
+            System.out.println("No grades available for the class.");
+        }
+    }
 }
