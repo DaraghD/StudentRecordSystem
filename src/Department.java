6e20bcc61513ba0d56e6a4bb3c58767987771a62
D-Money16 <22359621@studentmail.ul.ie>
2023-11-24 01:39:22 +0000

    Added method to display failing students in a module

--- src/Department.java
+++ src/Department.java
@@ -41,6 +41,37 @@ public class Department {
             System.out.println("Average QCA for module " + moduleCode + ": " + moduleAverage);
         } else {
             System.out.println("No grades available for the specified module.");
+        }}
+
+
+        public void displayFailedStudentsForModule(String moduleCode) {
+            System.out.println("Students who failed module " + moduleCode + ":");
+
+            //Iterate over students
+            for (Student student : this.university.getStudents()) {
+                double totalSum = 0.0;
+                int totalGradesCount = 0;
+
+                // Iterate over grades of each student
+                for (Grade grade : student.getGrades()) {
+                    // Check if the module matches the specified moduleCode
+                    if (grade.getModule().equals(moduleCode)) {
+                        // Check the grade using if loops and convertGradeToNumber method
+                        totalSum += grade.convertGradeToNumber();
+                        totalGradesCount++;
+                    }
+                }
+
+                //Calculate the QCA for the module
+                double moduleQCA = (totalGradesCount > 0) ? (totalSum / totalGradesCount) : 0.0;
+
+                //Check if the moduleQCA is less than 2.0 (failure)
+                if (moduleQCA < 2.0) {
+                    System.out.println("Student ID: " + student.getId() + ", Name: " + student.getName());
+                }
+            }
         }
+
     }
-}
+
+
