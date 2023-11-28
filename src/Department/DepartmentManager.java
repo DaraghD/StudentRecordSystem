d74919ce3fa796f8eff68d90e99f5592a273b95b
JoeC <joeconsidine9@gmail.com>
2023-11-28 23:36:09 +0000

    DepartmentManager, University and StudentRecordSystem javadocs

--- src/Department/DepartmentManager.java
+++ src/Department/DepartmentManager.java
@@ -3,14 +3,30 @@ package Department;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Manages departments and allows teachers to view and add departments to the department board.
+ */
 public class DepartmentManager {
+    /**
+     * The name of the department manager.
+     */
     private String departmentManager;
+
+    /**
+     * The list of departments managed by the department manager.
+     */
     private List<String> departmentList;
+
+    /**
+     * Constructs a new DepartmentManager.
+     */
     public DepartmentManager() {
-        this.departmentManager = departmentManager;
         departmentList = new ArrayList<>();
     }
 
+    /**
+     * Displays the names of departments and allows users to view the department board.
+     */
     public void viewDepartmentBoard() {
         System.out.println("Department Board:");
         for (String department : departmentList) {
@@ -18,11 +34,22 @@ public class DepartmentManager {
         }
     }
 
+    /**
+     * Adds a new department to the department board.
+     *
+     * @param newDepartment The name of the new department to be added.
+     */
     public void addDepartment(String newDepartment) {
         departmentList.add(newDepartment);
         System.out.println("Added new department: " + newDepartment);
     }
-    public static void main (String[] args) {
+
+    /**
+     * The entry point for the DepartmentManager application.
+     *
+     * @param args The command-line arguments.
+     */
+    public static void main(String[] args) {
         DepartmentManager departmentManager = new DepartmentManager();
 
         // Adding departments
