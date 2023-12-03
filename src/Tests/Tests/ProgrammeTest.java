89ab751b332ee2a75b411280922e03b808ba4bd3
D-Money16 <22359621@studentmail.ul.ie>
2023-12-03 19:44:40 +0000

    programme testing

--- src/Tests/Tests/ProgrammeTest.java
+++ src/Tests/Tests/ProgrammeTest.java
@@ -1,4 +1,57 @@
-import static org.junit.jupiter.api.Assertions.*;
-class ProgrammeTest {
-  
+package Tests;
+
+import Grading.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ProgrammeTest {
+
+    private Programme programme;
+    private Module module;
+    private Student student;
+    private University university;
+    private Department department;
+
+    @Before
+    public void setUp() {
+        university = new University("Test University");
+        department = new Department("Test Department", university);
+        programme = new Programme("Test Programme", university, 3, ProgrammeType.UNDERGRADUATE, 60.0, department);
+        module = new Module("Test Module", "TM001", 20, department);
+        student = new Student("John Doe", "JD001", department);
+    }
+
+    @Test
+    public void testAddModule() {
+        programme.addModule(module);
+        assertTrue(programme.getModules().contains(module));
+    }
+
+    @Test
+    public void testGetModule() {
+        programme.addModule(module);
+        Module retrievedModule = programme.getModule("Test Module");
+        assertNotNull(retrievedModule);
+        assertEquals("Test Module", retrievedModule.getName());
+    }
+
+    @Test
+    public void testAddStudent() {
+        programme.addStudent(student);
+        assertTrue(programme.getStudents().contains(student));
+    }
+
+    @Test
+    public void testCsvFormat() {
+        String expectedCsvFormat = "Test Programme,3,UNDERGRADUATE,60.0,Test Department";
+        assertEquals(expectedCsvFormat, programme.csvFormat());
+    }
+
+    @Test
+    public void testCsvHeader() {
+        String expectedCsvHeader = "Name,Duration,Level,Cutoff,Department";
+        assertEquals(expectedCsvHeader, programme.csvHeader());
+    }
 }
