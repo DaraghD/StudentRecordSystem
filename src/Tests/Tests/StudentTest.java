e69fcd88b21197a77acdcc5ca396be11db7569fb
Caylum <caylumhurley@gmail.com>
2023-12-03 23:16:32 +0000

    Minor fixes to StudentTest.

--- src/Tests/Tests/StudentTest.java
+++ src/Tests/Tests/StudentTest.java
@@ -4,7 +4,7 @@ import Department.Department;
 import Grading.*;
 import Grading.Module;
 import Person.Student;
-import University.University;
+import University.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
