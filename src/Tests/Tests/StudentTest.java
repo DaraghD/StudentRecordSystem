050d90871873a0c41ac9adabd725b681a91b0121
Caylum <caylumhurley@gmail.com>
2023-12-03 17:55:12 +0000

    Created test case in StudentTest.

--- src/Tests/Tests/StudentTest.java
+++ src/Tests/Tests/StudentTest.java
@@ -1,14 +1,27 @@
 package Tests;
 
+import Department.Department;
+import Grading.Programme;
+import Grading.ProgrammeType;
+import Person.Student;
+import University.University;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class StudentTest {
+Student student = new Student("Caylum", 22356363, "Password123");
 
+University UL = new University();
+
+ProgrammeType level = new ProgrammeType();
+
+Department Science = new Department("Science", UL);
+
+Programme LM121 = new Programme("Foundations of Computer Science", UL, 4, 8, 2.0, Science);
     @Test
     void setProgramme() {
-        assertEquals();
+        assertEquals(student.setProgramme(LM121);
     }
 
     @Test
